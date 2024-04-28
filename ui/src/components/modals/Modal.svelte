31eae14da566ca7fc81ead160ce6d8e6b4b1ed5d
taylor-giles <taylor.g1823@gmail.com>
2024-04-27 21:13:43 -0400

    Fix modal blur close behavior

--- ui/src/components/modals/Modal.svelte
+++ ui/src/components/modals/Modal.svelte
@@ -6,33 +6,28 @@
     //Determines whether or not the modal should close when the user clicks outside of its content
     export let closeOnBlur = true;
 
+    let outerDiv: HTMLDivElement;
+
     /**
-     * Closes the modal when content loses focus
-     * @param event The blur event
+     * Fires the close event if the user clicks outside of the content
+     * @param event
      */
-    function handleBlur(
-        event: FocusEvent & {
-            currentTarget: EventTarget & HTMLDivElement;
-        },
-    ) {
-        if (
-            closeOnBlur &&
-            !event.currentTarget?.contains(event.relatedTarget as Node)
-        ) {
+    function handleCloseClick(event: MouseEvent) {
+        if (event.target === outerDiv && closeOnBlur) {
             dispatch("close");
         }
     }
 </script>
 
-<div class="modal" transition:blur={{ duration: 100 }}>
-    <!-- svelte-ignore a11y-autofocus -->
-    <div
-        id="modal-content-wrapper"
-        style="outline: none;"
-        tabindex="-1"
-        autofocus
-        on:blur={handleBlur}
-    >
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<!-- svelte-ignore a11y-no-static-element-interactions -->
+<div
+    class="modal"
+    transition:blur={{ duration: 100 }}
+    on:click={handleCloseClick}
+    bind:this={outerDiv}
+>
+    <div id="modal-content-wrapper" style="outline: none;" tabindex="-1">
         <slot />
     </div>
 </div>
