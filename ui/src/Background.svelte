158948ba5c604f5f40f509a9cf5133b8c53b5014
taylor-giles <taylor.g1823@gmail.com>
2024-05-22 17:00:55 -0400

    Stop generating background floaters when tab is hidden

--- ui/src/Background.svelte
+++ ui/src/Background.svelte
@@ -6,78 +6,80 @@
         "circle.svg",
         "circle.svg",
         "music-circle.svg",
-        "bop-bash-single-color.svg"
+        "bop-bash-single-color.svg",
     ];
 
     let canvas: HTMLDivElement;
 
     async function generateNewFloater(startingVisibility: number = 0) {
-        //Choose a random shape
-        let shape = _.sample(FLOATER_SHAPES);
+        if (!document.hidden) {
+            //Choose a random shape
+            let shape = _.sample(FLOATER_SHAPES);
 
-        //Choose random start and end positions
-        let startX = _.random(-10, 110);
-        let startY = _.random(-10, 110);
-        let endX = _.random(-10, 110);
-        let endY = _.random(-10, 110);
+            //Choose random start and end positions
+            let startX = _.random(-10, 110);
+            let startY = _.random(-10, 110);
+            let endX = _.random(-10, 110);
+            let endY = _.random(-10, 110);
 
-        //Choose random start and end rotations
-        let startDeg = _.random(0, 720);
-        let endDeg = _.random(0, 720);
+            //Choose random start and end rotations
+            let startDeg = _.random(0, 720);
+            let endDeg = _.random(0, 720);
 
-        //Choose random duration
-        let duration = _.random(30000, 90000);
+            //Choose random duration
+            let duration = _.random(30000, 90000);
 
-        //Choose random size (in rem)
-        let imgSize = _.random(3, 20);
+            //Choose random size (in rem)
+            let imgSize = _.random(3, 20);
 
-        //Generate animation keyframes
-        let keyframes: Keyframe[] = [
-            {
-                opacity: startingVisibility,
-                height: `${startingVisibility * imgSize}px`,
-                width: `${startingVisibility * imgSize}px`,
-                left: `${startX}%`,
-                top: `${startY}%`,
-                transform: `rotate(${startDeg}deg)`,
-            },
-            {
-                opacity: 1,
-                height: `${imgSize}rem`,
-                width: `${imgSize}rem`,
-            },
-            {
-                opacity: 0,
-                height: "0px",
-                width: "0px",
-                left: `${endX}%`,
-                top: `${endY}%`,
-                transform: `rotate(${endDeg}deg)`,
-            },
-        ];
+            //Generate animation keyframes
+            let keyframes: Keyframe[] = [
+                {
+                    opacity: startingVisibility,
+                    height: `${startingVisibility * imgSize}px`,
+                    width: `${startingVisibility * imgSize}px`,
+                    left: `${startX}%`,
+                    top: `${startY}%`,
+                    transform: `rotate(${startDeg}deg)`,
+                },
+                {
+                    opacity: 1,
+                    height: `${imgSize}rem`,
+                    width: `${imgSize}rem`,
+                },
+                {
+                    opacity: 0,
+                    height: "0px",
+                    width: "0px",
+                    left: `${endX}%`,
+                    top: `${endY}%`,
+                    transform: `rotate(${endDeg}deg)`,
+                },
+            ];
 
-        //Generate HTML element for the floater
-        let floaterElement = document.createElement("img");
+            //Generate HTML element for the floater
+            let floaterElement = document.createElement("img");
 
-        //Apply style properties
-        floaterElement.src = `floaters/${shape}`;
-        floaterElement.style.zIndex = "-100";
-        floaterElement.style.position = "absolute";
-        floaterElement.style.height = `${startingVisibility * imgSize}px`;
-        floaterElement.style.width = `${startingVisibility * imgSize}px`;
+            //Apply style properties
+            floaterElement.src = `floaters/${shape}`;
+            floaterElement.style.zIndex = "-100";
+            floaterElement.style.position = "absolute";
+            floaterElement.style.height = `${startingVisibility * imgSize}px`;
+            floaterElement.style.width = `${startingVisibility * imgSize}px`;
 
-        //Add DOM element and animate it
-        await tick();
-        canvas?.appendChild(floaterElement);
-        await tick();
-        let animation = floaterElement.animate(keyframes, duration)
-        
-        //Remove DOM element after animation is finished
-        animation.onfinish = () => floaterElement?.remove();
+            //Add DOM element and animate it
+            await tick();
+            canvas?.appendChild(floaterElement);
+            await tick();
+            let animation = floaterElement.animate(keyframes, duration);
+
+            //Remove DOM element after animation is finished
+            animation.onfinish = () => floaterElement?.remove();
+        }
     }
 
     //Make 5 floaters right away
-    for(let i=0; i<5; i++){
+    for (let i = 0; i < 5; i++) {
         generateNewFloater(0.1 * i);
     }
 
